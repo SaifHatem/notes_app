37749d4e9495f5ee31a44f5252e4860043c9fea3
saif hatem <shatem793@gmail.com>
2024-08-01 17:03:26 +0300

    create custom text field

--- lib/widgets/add_note_botton_sheet.dart
+++ lib/widgets/add_note_botton_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteModalSheet extends StatelessWidget {
   const AddNoteModalSheet({super.key});
@@ -10,6 +11,23 @@ class AddNoteModalSheet extends StatelessWidget {
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(12),
           topRight: Radius.circular(12),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 16),
+        child: Column(
+          children: [
+            CustomTextField(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+
+  final String hint;
+  final int maxLines;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      textAlign: TextAlign.left,
+      textAlignVertical: TextAlignVertical.center,
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(
+        12,
+      ),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
