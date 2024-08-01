a3b0735980e781bae7c8fb0f9849b2fab76af6bb
saif hatem <shatem793@gmail.com>
2024-08-01 19:48:04 +0300

    take input and validate data

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,24 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text});
+  const CustomButton({super.key, required this.text, this.onTap});
 
   final String text;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // Better than double.infinity
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w300),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        // Better than double.infinity
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w300),
+          ),
         ),
       ),
     );
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
 
   final String hint;
   final int maxLines;
+  final Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        //! حطيت علامة الاستفهام عشان انا مش ضامن انه مش بنل , ولو مكنتش ب نل ف خلاص خد ترو ف هيجع السترينج
+        if (value?.isEmpty ?? true) {
+          return 'This Field is required';
+        }
+        return null;
+      },
       maxLines: maxLines,
       textAlign: TextAlign.left,
       textAlignVertical: TextAlignVertical.center,
