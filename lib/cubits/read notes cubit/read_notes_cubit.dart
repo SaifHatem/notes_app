80bb4a5495e4880fcd7a3421c741e9c42bf6cc18
saif hatem <shatem793@gmail.com>
2024-08-02 18:58:20 +0300

    provide read cubit

--- lib/cubits/read notes cubit/read_notes_cubit.dart
+++ lib/cubits/read notes cubit/read_notes_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  readNotes() async {
+    emit(ReadNotesLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+
+      List<NoteModel> notes = noteBox.values.toList();
+
+      emit(ReadNotesSuccess(notes));
+    } catch (e) {
+      emit(ReadNotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/read%20notes%20cubit/read_notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_botton_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -9,25 +11,28 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: SafeArea(
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
 
-                //! دي عشان احل مشكلة الكيبورد لما بتظهر
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                context: context,
-                builder: (context) {
-                  return const AddNoteModalSheet();
-                });
-          },
-          child: const Icon(Icons.add),
+                  //! دي عشان احل مشكلة الكيبورد لما بتظهر
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return const AddNoteModalSheet();
+                  });
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: const NotesViewBody(),
         ),
-        body: const NotesViewBody(),
       ),
     );
   }
